84d091c2493d223cf24f1910e1b3bfdf5c121297
Ido.Slonimsky <islonimsky@ironscales.com>
2023-04-23 15:30:23 +0300

    Initial markdown read support

--- src/components/pages/blog.rs
+++ src/components/pages/blog.rs
@@ -1,7 +1,40 @@
+use include_dir::{include_dir, Dir};
+use log::info;
 use yew::prelude::*;
+
+const POSTS_DIR: Dir = include_dir!("posts");
+
+#[derive(Debug)]
+struct PostTitle {
+    title: String,
+    date: String,
+    html: String,
+}
+
+fn get_field(field: &str, contents: &str) -> String {
+    if let Some(title_line) = contents.lines().find(|line| line.starts_with(field)) {
+        if let Some(title) = title_line.split(":").nth(1) {
+            return title.trim().trim_matches('"').to_owned();
+        } else {
+            return "".to_owned();
+        }
+    }
+    "".to_owned()
+}
 
 #[function_component(Blog)]
 pub fn blog() -> Html {
+    let mut posts: Vec<PostTitle> = Vec::new();
+
+    for file in POSTS_DIR.files() {
+        let contents = file.contents_utf8().unwrap_or("");
+        posts.push(PostTitle {
+            title: get_field("title", contents),
+            date: get_field("date", contents),
+            html: markdown::to_html(contents),
+        });
+    }
+    info!("{:?}", posts);
     html! {
         <h1>{ "Blog" }</h1>
     }
